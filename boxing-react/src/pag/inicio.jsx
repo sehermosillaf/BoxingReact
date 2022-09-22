bb13170fe3a8df0135a3c3a5c736312d43484633
Sebastian <sehermosillaf@gmail.com>
2022-09-22 17:03:09 -0300

    Corrige Footer

--- boxing-react/src/pag/inicio.jsx
+++ boxing-react/src/pag/inicio.jsx
@@ -1,13 +1,12 @@
 import { Disciplinas } from "./disciplinas"
 import { CarouselInicio } from "../componentes/Carousel/carousel"
-import Footer from "../componentes/Footer/footer";
+
 
 export const Inicio = () => {
     return (
    <div>
     <CarouselInicio/>
     <Disciplinas/>
-    <Footer/>
    </div>
     );
 }
