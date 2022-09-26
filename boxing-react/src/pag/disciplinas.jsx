54706b25057b13e26c2cd1e84b41eb0af134d68d
Carlos Basaez <56442908+cxvetown@users.noreply.github.com>
2022-09-26 15:07:07 -0300

    pagina de inicio

--- boxing-react/src/pag/disciplinas.jsx
+++ boxing-react/src/pag/disciplinas.jsx
@@ -11,23 +11,23 @@ export const Disciplinas = () => {
       <CardGroup className="mx-auto mt-5" style={{ width: "80%" }}>
         <CardComponent
           titulo="Boxeo"
-          desc="Deporte de contacto basado en golpes de puño sobre la cintura.Alto trabajo aeróbico, combinado con coordinación y técnica."
+          desc="Deporte de contacto basado en golpes de puño sobre la cintura, posee un alto trabajo aeróbico, combinado con coordinación y técnica."
           img={Boxeo}
-          linkText="Mas informacion"
+          linkText="Mas información"
         />
 
         <CardComponent
           titulo="KickBoxing"
           desc="Disciplina catalogada como arte marcial, puedes progresar mediante sistema de grados o cinturones.Combinación de golpes de puño y patadas."
           img={KickBoxing}
-          linkText="Mas informacion"
+          linkText="Mas información"
         />
         <CardComponent
           titulo="Fitness Training"
           desc="Trabajo físico GAP (glúteos, abdomen y piernas)  focalizado.
 HIT, entrenamiento de alta intensidad clave para mejorar el estado físico y ponerse en forma."
           img={Fitness}
-          linkText="Mas informacion"
+          linkText="Mas información"
         />
       </CardGroup>
 
